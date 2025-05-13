cf71547564e572bcf6bd1cf8f8c25c1c18a2074b
Vlad Grigorev <vladgrigorev94@gmail.com>
2025-05-13 17:11:14 +0700

    правки

--- main.go
+++ main.go
@@ -26,7 +26,7 @@ func streamHandler(w http.ResponseWriter, r *http.Request) {
 
 	directURL := strings.TrimSpace(string(output))
 	//directURL = strings.TrimSpace(directURL)
-	//directURL = strings.ReplaceAll(directURL, "\n", "")
+	directURL = strings.ReplaceAll(directURL, "\n", "")
 	//directURL = strings.ReplaceAll(directURL, "\t", "")
 	log.Println("Direct URL:", directURL)
 
